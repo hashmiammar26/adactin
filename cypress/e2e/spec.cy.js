5842ac0037e208a9827508a4bee108e77a23ecec
hashmiammar26 <syedammar.alihashmi@gmail.com>
2024-12-18 19:00:59 +0500

    single page testcases

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,75 @@
+describe('Adactin Hotel App Tests', () => {
+  const performValidLogin = () => {
+    urlfunction();
+    cy.get('#username').type("asfandtariq");
+    cy.get('#password').type("159159..");
+    cy.get('#login').click();
+    cy.xpath("/html/body/table[2]/tbody/tr[1]/td[1]").should('have.text', 'Welcome to Adactin Group of Hotels');
+  };
+
+  const urlfunction = () => {
+    cy.visit('https://adactinhotelapp.com/');
+  };
+
+  const search_hotel = () => {
+    performValidLogin();
+    cy.get('#location').select('Sydney'); // Select a location
+    cy.get('#hotels').select('Hotel Creek'); // Select a hotel
+    cy.get('#Submit').click(); // Submit the form
+    cy.xpath("/html/body/table[2]/tbody/tr[2]/td/form/table/tbody/tr[1]/td").should('be.visible')}; // Assert that the next page loads
+    
+
+    const selecthotel = () => {
+      search_hotel();
+      cy.get('#radiobutton_1').click();
+      cy.get('#continue').click();
+      cy.xpath("/html/body/table[2]/tbody/tr[2]/td/form/table/tbody/tr[2]/td").should('be.visible'); //assert
+
+    }
+
+    const bookhotel = () => {
+      selecthotel();
+      cy.get('#first_name').type('Ammar');
+      cy.get('#last_name').type('Hashmi');
+      cy.get('#address').type('2nd Floor, Z House, Mansoor Tower, Clifton Block 8, Karachi, Pakistan');
+      cy.get('#cc_num').type('1234567887654321');
+      cy.get('#cc_type').select('Other');
+      cy.get('#cc_exp_month').select('May');
+      cy.get('#cc_exp_year').select('2024');
+      cy.get('#cc_cvv').type('008');
+      cy.get('#book_now').click();
+      cy.wait(5000);
+      cy.xpath("/html/body/table[2]/tbody/tr[2]/td/form/table/tbody/tr[1]/td").should('be.visible'); //assert
+      cy.get('#logout').click();
+      cy.xpath("/html/body/table[2]/tbody/tr/td[1]/table/tbody/tr/td/a").click();
+      cy.xpath("/html/body/table[2]/tbody/tr/td[1]/p").should('be.visible'); //assert
+    }
+
+  it('Invalid login', () => {
+    urlfunction();
+    cy.get('#username').type("ammarh");
+    cy.get('#password').type("Ammar123!");
+    cy.get('#login').click();
+    cy.xpath("/html/body/table[2]/tbody/tr/td[2]/form/table/tbody/tr[5]/td[2]/div/b")
+      .should('have.text', 'Invalid Login details or Your Password might have expired. Click here to reset your password');
+  });
+
+  it('Valid login', () => {
+    performValidLogin();
+  });
+
+  it('Search hotel', () => {
+    search_hotel();
+});
+
+it('Select hotel', () => {
+  selecthotel();
+})
+  
+it('book hotel', () => {
+  bookhotel();
+})
+
+});
+
+
